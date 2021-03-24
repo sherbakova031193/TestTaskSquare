410cc3cf291b2697ba9b516453ccdcb34c143f6c
Елизавeта <sherbakova031193@gmail.com>
2021-03-24 13:24:19 +0300

    Added: - Square animation

---
+++ TestTask/UIView + Extensions.swift
@@ -0,0 +1,11 @@
+//
+//  UIView + Extensions.swift
+//  TestTask
+//
+//  Created by Елизавета Щербакова on 24.03.2021.
+//
+
+import UIKit
+
+
+
--- TestTask/ViewController.swift
+++ TestTask/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private var squareView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureSubviews()
     }
-
-
+    
+    override func viewDidLayoutSubviews() {
+        configureAnimate()
+    }
+    
+    private func configureSubviews() {
+        view.backgroundColor = .white
+        squareView = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        squareView.center = view.center
+        squareView.backgroundColor = .cyan
+        view.addSubview(squareView)
+    }
+    
+    private func configureAnimate() {
+        UIView.animate(withDuration: 0.3, delay: 0.3, options: .curveEaseOut) {
+            self.squareView.transform = CGAffineTransform(scaleX: 2.0, y: 2.0)
+        }
+    }
 }
 
